9a686e9252cbc8f6f0f6e2081d27cdca7c919086
nmarks3 <nmarks3@L11705042768.wctc.msft>
2015-04-16 17:01:19 -0500

    cujo

---
+++ src/tempconverter/TempStartup.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tempconverter;
+
+import javax.swing.UIManager;
+
+/**
+ *
+ * @author nmarks3
+ */
+public class TempStartup {
+    public static void main(String[] args) {
+        try {
+            // Set System L&F
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            // do nothing, this can safely be ignored because
+            // it will fall back to the basic metal L&F if something
+            // goes wrong. This is also a rare example of a scenario
+            // where the catch clause could be safely eliminated.
+        }
+         java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new TempGUI().setVisible(true);
+              
+            }
+        });
+    }
+}
